f7046f2afc7e6919b96cb2e10a503a64907f0e86
Biszterszky Mátyás <matyi0918@gmail.com>
2020-11-15 10:02:06 +0100

    gitignore update

--- dist/main.js
+++ dist/main.js
@@ -20,13 +20,22 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var Discord = __importStar(require("discord.js"));
+var express = require('express');
+var app = express();
 var dotenv_1 = require("dotenv");
 dotenv_1.config();
 var client = new Discord.Client();
 console.log(process.env.SECRET);
+var port = process.env.PORT;
+console.log("Port is " + port);
 client.once('ready', function () {
     console.log('Kádár is ready to fight');
 });
-console.log(process.env.SECRET);
 client.login(process.env.SECRET).then(function (r) {
+});
+app.get('/', function (req, res) {
+    res.send('Kedves elvtárs, ....');
+});
+app.listen(port, function () {
+    console.log("Example app listening at http://localhost:" + port);
 });
--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 import * as Discord from "discord.js";
+const express = require('express')
+const app = express()
 
 import {config} from "dotenv";
 config();
@@ -7,12 +9,24 @@ config();
 const client = new Discord.Client();
 
 console.log(process.env.SECRET);
+const port = process.env.PORT;
+console.log(`Port is ${port}`);
 
 client.once('ready', () => {
     console.log('Kádár is ready to fight')
 })
 
-console.log(process.env.SECRET);
 
 client.login(process.env.SECRET).then(r => {
 });
+
+
+
+app.get('/', (req: any, res: any) => {
+    res.send('Kedves elvtárs, ....')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
